1a6f830f970797fe856faf031b938c65cfa70425
SpiffGreen <spiffjekeygreen@gmail.com>
2024-08-30 13:37:18 +0100

    fix: test new update

--- nodes.py
+++ nodes.py
@@ -1,4 +1,6 @@
-from PIL import Image
+import torch
+from PIL import Image, ImageOps
+import numpy as np
 
 """
 The ComfyUI Meshanythingv2 Node simply takes an input image/text/3d Object and turns into a mesh, even smaller size.
@@ -8,19 +10,39 @@ class MeshImage:
   def INPUT_TYPES(s):
     return {
 			"required": {
-				"image": ("IMAGE",),
+				"images": ("IMAGE",),
 			}
 		}
     
   RETURN_TYPES = ("IMAGE",)
-  FUNCTION = "mesh_image"
+  FUNCTION = "mesh_images"
 
   CATEGORY = "image/postprocessing"
   
-  def mesh_image(self, image):
-    converted_image = Image.fromarray(image)
-    converted_image = converted_image.convert("L")
-    return converted_image
+  def mesh_images(self, images):
+    converted_images = []
+    for image in images:
+      i = 255. * image.cpu().numpy()
+      img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8)).convert("L")
+      converted_images.append(img)
+      
+    images = []
+    for pil_image in converted_images:
+        i = pil_image
+        i = ImageOps.exif_transpose(i)
+        if i.mode == 'I':
+            i = i.point(lambda i: i * (1 / 255))
+        image = i.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        images.append(image)
+    
+    if len(images) > 1:
+        images = torch.cat(images, dim=0)
+    else:
+        images = images[0]
+
+    return (images,)
     
 NODE_CLASS_MAPPINGS = {
     "CMA_MeshImage": MeshImage,
