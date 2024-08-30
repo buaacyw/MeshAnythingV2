078d10c3f7b1f374f15edb411cbeebea3dced5a7
SpiffGreen <spiffjekeygreen@gmail.com>
2024-08-30 08:47:49 +0100

    fix: return types

--- __init__.py
+++ __init__.py
@@ -2,11 +2,6 @@ from .comfyui_meshanything_v2 import MeshImage
 
 NODE_CLASS_MAPPINGS = {
     "MeshImage": MeshImage,
-    # "MeshText": MeshText,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "MeshImage": "MeshImage 2"
 }
 
 print('--------------')
@@ -14,4 +9,4 @@ print('*ComfyUI_MeshAnythingV2- nodes_loaded*')
 print('--------------')
 
 # __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-__ALL__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__ALL__ = ['NODE_CLASS_MAPPINGS']
