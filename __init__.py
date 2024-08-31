a26a2bd0013b4cb65baf8e74e72e260c4f03cd58
SpiffGreen <spiffjekeygreen@gmail.com>
2024-08-31 21:59:49 +0100

    test: update imports for meshanything models

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import importlib
 
-nodes_module = importlib.import_module(".nodes", package=__name__)
+nodes_module = importlib.import_module("nodes", package=__name__)
 NODE_CLASS_MAPPINGS = getattr(nodes_module, "NODE_CLASS_MAPPINGS")
 
 print('--------------')
--- nodes.py
+++ nodes.py
@@ -55,95 +55,95 @@ class GrayScale:
         return (image,)
 
 
-class MeshImage:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "image": ("IMAGE",),
-            }
-        }
+# class MeshImage:
+#     @classmethod
+#     def INPUT_TYPES(s):
+#         return {
+#             "required": {
+#                 "image": ("IMAGE",),
+#             }
+#         }
 
-    RETURN_TYPES = ("MESH",)
-    RETURN_NAMES = ("mesh",)
-    FUNCTION = "mesh_image"
-    OUTPUT_NODE = True
+#     RETURN_TYPES = ("MESH",)
+#     RETURN_NAMES = ("mesh",)
+#     FUNCTION = "mesh_image"
+#     OUTPUT_NODE = True
 
-    CATEGORY = "MeshAnythingV2/process"
+#     CATEGORY = "MeshAnythingV2/process"
 
-    def mesh_image(self, image):
-        cur_time = datetime.datetime.now().strftime("%d_%H-%M-%S")
-        checkpoint_dir = os.path.join(os.getcwd(), cur_time)
-        os.makedirs(checkpoint_dir, exist_ok=True)
-        kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-        accelerator = Accelerator(
-            mixed_precision="fp16", project_dir=checkpoint_dir, kwargs_handlers=[kwargs]
-        )
-        model = MeshAnythingV2.from_pretrained("Yiwen-ntu/meshanythingv2")
-        set_seed(0)
-        dataset = Dataset("pc_normal", [MeshImage._resolve_path(image=image)], False, 7)
+#     def mesh_image(self, image):
+#         cur_time = datetime.datetime.now().strftime("%d_%H-%M-%S")
+#         checkpoint_dir = os.path.join(os.getcwd(), cur_time)
+#         os.makedirs(checkpoint_dir, exist_ok=True)
+#         kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+#         accelerator = Accelerator(
+#             mixed_precision="fp16", project_dir=checkpoint_dir, kwargs_handlers=[kwargs]
+#         )
+#         model = MeshAnythingV2.from_pretrained("Yiwen-ntu/meshanythingv2")
+#         set_seed(0)
+#         dataset = Dataset("pc_normal", [MeshImage._resolve_path(image=image)], False, 7)
 
-        # Start ---------
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=1,
-            drop_last=False,
-            shuffle=False,
-        )
+#         # Start ---------
+#         dataloader = torch.utils.data.DataLoader(
+#             dataset,
+#             batch_size=1,
+#             drop_last=False,
+#             shuffle=False,
+#         )
 
-        if accelerator.state.num_processes > 1:
-            model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+#         if accelerator.state.num_processes > 1:
+#             model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
-        dataloader, model = accelerator.prepare(dataloader, model)
-        begin_time = time.time()
-        print("Generation Start!!!")
+#         dataloader, model = accelerator.prepare(dataloader, model)
+#         begin_time = time.time()
+#         print("Generation Start!!!")
 
-        with accelerator.autocast():
-            for curr_iter, batch_data_label in enumerate(dataloader):
-                outputs = model(batch_data_label["pc_normal"], sampling=False)
-                batch_size = outputs.shape[0]
-                device = outputs.device
+#         with accelerator.autocast():
+#             for curr_iter, batch_data_label in enumerate(dataloader):
+#                 outputs = model(batch_data_label["pc_normal"], sampling=False)
+#                 batch_size = outputs.shape[0]
+#                 device = outputs.device
 
-                for batch_id in range(batch_size):
-                    recon_mesh = outputs[batch_id]
-                    valid_mask = torch.all(
-                        ~torch.isnan(recon_mesh.reshape((-1, 9))), dim=1
-                    )
-                    recon_mesh = recon_mesh[valid_mask]  # nvalid_face x 3 x 3
+#                 for batch_id in range(batch_size):
+#                     recon_mesh = outputs[batch_id]
+#                     valid_mask = torch.all(
+#                         ~torch.isnan(recon_mesh.reshape((-1, 9))), dim=1
+#                     )
+#                     recon_mesh = recon_mesh[valid_mask]  # nvalid_face x 3 x 3
 
-                    vertices = recon_mesh.reshape(-1, 3).cpu()
-                    vertices_index = np.arange(len(vertices))  # 0, 1, ..., 3 x face
-                    triangles = vertices_index.reshape(-1, 3)
+#                     vertices = recon_mesh.reshape(-1, 3).cpu()
+#                     vertices_index = np.arange(len(vertices))  # 0, 1, ..., 3 x face
+#                     triangles = vertices_index.reshape(-1, 3)
 
-                    scene_mesh = trimesh.Trimesh(
-                        vertices=vertices,
-                        faces=triangles,
-                        force="mesh",
-                        merge_primitives=True,
-                    )
-                    scene_mesh.merge_vertices()
-                    scene_mesh.update_faces(scene_mesh.nondegenerate_faces())
-                    scene_mesh.update_faces(scene_mesh.unique_faces())
-                    scene_mesh.remove_unreferenced_vertices()
-                    scene_mesh.fix_normals()
-                    save_path = os.path.join(
-                        checkpoint_dir, f'{batch_data_label["uid"][batch_id]}_gen.obj'
-                    )
-                    num_faces = len(scene_mesh.faces)
-                    brown_color = np.array([255, 165, 0, 255], dtype=np.uint8)
-                    face_colors = np.tile(brown_color, (num_faces, 1))
+#                     scene_mesh = trimesh.Trimesh(
+#                         vertices=vertices,
+#                         faces=triangles,
+#                         force="mesh",
+#                         merge_primitives=True,
+#                     )
+#                     scene_mesh.merge_vertices()
+#                     scene_mesh.update_faces(scene_mesh.nondegenerate_faces())
+#                     scene_mesh.update_faces(scene_mesh.unique_faces())
+#                     scene_mesh.remove_unreferenced_vertices()
+#                     scene_mesh.fix_normals()
+#                     save_path = os.path.join(
+#                         checkpoint_dir, f'{batch_data_label["uid"][batch_id]}_gen.obj'
+#                     )
+#                     num_faces = len(scene_mesh.faces)
+#                     brown_color = np.array([255, 165, 0, 255], dtype=np.uint8)
+#                     face_colors = np.tile(brown_color, (num_faces, 1))
 
-                    scene_mesh.visual.face_colors = face_colors
-                    scene_mesh.export(save_path)
-                    print(f"{save_path} Over!!")
-        end_time = time.time()
-        print(f"Total time: {end_time - begin_time}")
+#                     scene_mesh.visual.face_colors = face_colors
+#                     scene_mesh.export(save_path)
+#                     print(f"{save_path} Over!!")
+#         end_time = time.time()
+#         print(f"Total time: {end_time - begin_time}")
 
-        return (scene_mesh,)
+#         return (scene_mesh,)
 
-    def _resolve_path(image) -> Path:
-        image_path = Path(folder_paths.get_annotated_filepath(image))
-        return image_path
+#     def _resolve_path(image) -> Path:
+#         image_path = Path(folder_paths.get_annotated_filepath(image))
+#         return image_path
 
 
 class SaveMesh:
@@ -180,7 +180,7 @@ class SaveMesh:
 
 
 NODE_CLASS_MAPPINGS = {
-    "CMA_MeshImage": MeshImage,
+    # "CMA_MeshImage": MeshImage,
     "CMA_SaveMesh": SaveMesh,
     "CMA_GrayScale": GrayScale,
 }
