3da0764af9647358a2959f3c40001db71b442856
SpiffGreen <spiffjekeygreen@gmail.com>
2024-08-31 22:16:08 +0100

    test: update imports for meshanything models

--- __init__.py
+++ __init__.py
@@ -2,7 +2,12 @@
 
 # nodes_module = importlib.import_module("nodes", package=__name__)
 # NODE_CLASS_MAPPINGS = getattr(nodes_module, "NODE_CLASS_MAPPINGS")
-from .nodes import NODE_CLASS_MAPPINGS
+from .nodes import SaveMesh, GrayScale
+
+NODE_CLASS_MAPPINGS = {
+    "CMA_SaveMesh": SaveMesh,
+    "CMA_GrayScale": GrayScale,
+}
 
 print('--------------')
 print('*ComfyUI_MeshAnythingV2- nodes_loaded*')
