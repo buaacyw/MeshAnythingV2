93a84205ad0504f81b3f9d8d421a9057b97aa23e
SpiffGreen <spiffjekeygreen@gmail.com>
2024-08-31 20:24:54 +0100

    test: 1

--- nodes.py
+++ nodes.py
@@ -10,116 +10,165 @@ from accelerate import Accelerator
 from accelerate.utils import set_seed
 from accelerate.utils import DistributedDataParallelKwargs
 from MeshAnything.models.meshanything_v2 import MeshAnythingV2
-from .utils import Dataset, conv_pil_tensor, conv_tensor_pil
+from .utils import Dataset, conv_pil_tensor, conv_tensor_pil, parse_save_filename
 
 
 """
 The ComfyUI Meshanythingv2 Node simply takes an input image/text/3d Object and turns into a mesh, even smaller size.
 """
-class GrayScale:
-  @classmethod
-  def INPUT_TYPES(s):
-    return {
-			"required": {
-				"image": ("IMAGE",),
-			}
-		}
-    
-  RETURN_TYPES = ("IMAGE",)
-  FUNCTION = "grayscale_image"
 
-  CATEGORY = "image/postprocessing"
-  
-  def grayscale_image(self, image):
-    image = conv_pil_tensor(conv_tensor_pil(image).convert("L"))
-    return (image,)
-   
+SUPPORTED_3D_EXTENSIONS = (
+    ".obj",
+    ".ply",
+    ".glb",
+)
+
+
+class GrayScale:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "grayscale_image"
+
+    CATEGORY = "MeshAnythingV2/process"
+
+    def grayscale_image(self, image):
+        image = conv_pil_tensor(conv_tensor_pil(image).convert("L"))
+        return (image,)
+
 
 class MeshImage:
-  @classmethod
-  def INPUT_TYPES(s):
-    return {
-			"required": {
-				"image": ("IMAGE",),
-			}
-		}
-    
-  RETURN_TYPES = ("IMAGE",)
-  FUNCTION = "mesh_image"
-  OUTPUT_NODE = True
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            }
+        }
 
-  CATEGORY = "image/postprocessing"
-  
-  def mesh_image(self, image):
-    cur_time = datetime.datetime.now().strftime("%d_%H-%M-%S")
-    checkpoint_dir = os.path.join(os.getcwd(), cur_time)
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-    accelerator = Accelerator(
-        mixed_precision="fp16",
-        project_dir=checkpoint_dir,
-        kwargs_handlers=[kwargs]
-    )
-    model = MeshAnythingV2.from_pretrained("Yiwen-ntu/meshanythingv2")
-    set_seed(0)
-    dataset = Dataset("pc_normal", [MeshImage._resolve_path(image=image)], False, 7)
-    
-    
-    # Start ---------
-    dataloader = torch.utils.data.DataLoader(
-      dataset,
-      batch_size=1,
-      drop_last = False,
-      shuffle = False,
-    )
+    RETURN_TYPES = ("MESH",)
+    RETURN_NAMES = ("mesh",)
+    FUNCTION = "mesh_image"
+    OUTPUT_NODE = True
 
-    if accelerator.state.num_processes > 1:
-      model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-    
-    dataloader, model = accelerator.prepare(dataloader, model)
-    begin_time = time.time()
-    print("Generation Start!!!")
-    
-    with accelerator.autocast():
-      for curr_iter, batch_data_label in enumerate(dataloader):
-        outputs = model(batch_data_label['pc_normal'], sampling=False)
-        batch_size = outputs.shape[0]
-        device = outputs.device
+    CATEGORY = "MeshAnythingV2/process"
 
-        for batch_id in range(batch_size):
-          recon_mesh = outputs[batch_id]
-          valid_mask = torch.all(~torch.isnan(recon_mesh.reshape((-1, 9))), dim=1)
-          recon_mesh = recon_mesh[valid_mask]  # nvalid_face x 3 x 3
+    def mesh_image(self, image):
+        cur_time = datetime.datetime.now().strftime("%d_%H-%M-%S")
+        checkpoint_dir = os.path.join(os.getcwd(), cur_time)
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+        accelerator = Accelerator(
+            mixed_precision="fp16", project_dir=checkpoint_dir, kwargs_handlers=[kwargs]
+        )
+        model = MeshAnythingV2.from_pretrained("Yiwen-ntu/meshanythingv2")
+        set_seed(0)
+        dataset = Dataset("pc_normal", [MeshImage._resolve_path(image=image)], False, 7)
 
-          vertices = recon_mesh.reshape(-1, 3).cpu()
-          vertices_index = np.arange(len(vertices))  # 0, 1, ..., 3 x face
-          triangles = vertices_index.reshape(-1, 3)
+        # Start ---------
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=1,
+            drop_last=False,
+            shuffle=False,
+        )
 
-          scene_mesh = trimesh.Trimesh(vertices=vertices, faces=triangles, force="mesh",
-                                        merge_primitives=True)
-          scene_mesh.merge_vertices()
-          scene_mesh.update_faces(scene_mesh.nondegenerate_faces())
-          scene_mesh.update_faces(scene_mesh.unique_faces())
-          scene_mesh.remove_unreferenced_vertices()
-          scene_mesh.fix_normals()
-          save_path = os.path.join(checkpoint_dir, f'{batch_data_label["uid"][batch_id]}_gen.obj')
-          num_faces = len(scene_mesh.faces)
-          brown_color = np.array([255, 165, 0, 255], dtype=np.uint8)
-          face_colors = np.tile(brown_color, (num_faces, 1))
+        if accelerator.state.num_processes > 1:
+            model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
-          scene_mesh.visual.face_colors = face_colors
-          scene_mesh.export(save_path)
-          print(f"{save_path} Over!!")
-    end_time = time.time()
-    print(f"Total time: {end_time - begin_time}")
-    
-    return (image,)
-  
-  def _resolve_path(image) -> Path:
-    image_path = Path(folder_paths.get_annotated_filepath(image))
-    return image_path
-    
+        dataloader, model = accelerator.prepare(dataloader, model)
+        begin_time = time.time()
+        print("Generation Start!!!")
+
+        with accelerator.autocast():
+            for curr_iter, batch_data_label in enumerate(dataloader):
+                outputs = model(batch_data_label["pc_normal"], sampling=False)
+                batch_size = outputs.shape[0]
+                device = outputs.device
+
+                for batch_id in range(batch_size):
+                    recon_mesh = outputs[batch_id]
+                    valid_mask = torch.all(
+                        ~torch.isnan(recon_mesh.reshape((-1, 9))), dim=1
+                    )
+                    recon_mesh = recon_mesh[valid_mask]  # nvalid_face x 3 x 3
+
+                    vertices = recon_mesh.reshape(-1, 3).cpu()
+                    vertices_index = np.arange(len(vertices))  # 0, 1, ..., 3 x face
+                    triangles = vertices_index.reshape(-1, 3)
+
+                    scene_mesh = trimesh.Trimesh(
+                        vertices=vertices,
+                        faces=triangles,
+                        force="mesh",
+                        merge_primitives=True,
+                    )
+                    scene_mesh.merge_vertices()
+                    scene_mesh.update_faces(scene_mesh.nondegenerate_faces())
+                    scene_mesh.update_faces(scene_mesh.unique_faces())
+                    scene_mesh.remove_unreferenced_vertices()
+                    scene_mesh.fix_normals()
+                    save_path = os.path.join(
+                        checkpoint_dir, f'{batch_data_label["uid"][batch_id]}_gen.obj'
+                    )
+                    num_faces = len(scene_mesh.faces)
+                    brown_color = np.array([255, 165, 0, 255], dtype=np.uint8)
+                    face_colors = np.tile(brown_color, (num_faces, 1))
+
+                    scene_mesh.visual.face_colors = face_colors
+                    scene_mesh.export(save_path)
+                    print(f"{save_path} Over!!")
+        end_time = time.time()
+        print(f"Total time: {end_time - begin_time}")
+
+        return (scene_mesh,)
+
+    def _resolve_path(image) -> Path:
+        image_path = Path(folder_paths.get_annotated_filepath(image))
+        return image_path
+
+
+class SaveMesh:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "mesh": ("MESH",),
+                "save_path": (
+                    "STRING",
+                    {"default": "Mesh_%Y-%m-%d-%M-%S-%f.glb", "multiline": False},
+                ),
+            },
+        }
+
+    OUTPUT_NODE = True
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("save_path",)
+    FUNCTION = "save_mesh"
+    CATEGORY = "MeshAnythingV2/Import|Export"
+
+    def save_mesh(self, mesh, save_path):
+        save_path = parse_save_filename(
+            save_path,
+            folder_paths.output_directory,
+            SUPPORTED_3D_EXTENSIONS,
+            self.__class__.__name__,
+        )
+
+        if save_path is not None:
+            mesh.write(save_path)
+
+        return (save_path,)
+
+
 NODE_CLASS_MAPPINGS = {
     "CMA_MeshImage": MeshImage,
+    "CMA_SaveMesh": SaveMesh,
     "CMA_GrayScale": GrayScale,
 }
--- utils.py
+++ utils.py
@@ -4,40 +4,93 @@ import torch
 import numpy as np
 from PIL import Image
 from numpy import asarray
+import os
+from os.path import isfile, join, exists, dirname
+import datetime
 
 # Utils
 
+
 def conv_pil_tensor(img):
-	return (torch.from_numpy(np.array(img).astype(np.float32) / 255.0).unsqueeze(0),)
+    return (torch.from_numpy(np.array(img).astype(np.float32) / 255.0).unsqueeze(0),)
+
 
 def conv_tensor_pil(tsr):
-	return Image.fromarray(np.clip(255. * tsr.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+    return Image.fromarray(
+        np.clip(255.0 * tsr.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
+    )
+
+
+def parse_save_filename(save_path, output_directory, supported_extensions, class_name):
+
+    folder_path, filename = os.path.split(save_path)
+    filename, file_extension = os.path.splitext(filename)
+    if file_extension.lower() in supported_extensions:
+        if not os.path.isabs(save_path):
+            folder_path = join(output_directory, folder_path)
+
+        os.makedirs(folder_path, exist_ok=True)
+
+        # replace time date format to current time
+        now = datetime.now()  # current date and time
+        all_date_format = ["%Y", "%m", "%d", "%H", "%M", "%S", "%f"]
+        for date_format in all_date_format:
+            if date_format in filename:
+                filename = filename.replace(date_format, now.strftime(date_format))
+
+        save_path = join(folder_path, filename) + file_extension
+        print(f"[{class_name}] Saving model to {save_path}")
+        return save_path
+    else:
+        print(
+            f"[{class_name}] File name {filename} does not end with supported file extensions: {supported_extensions}"
+        )
+
+    return None
+
 
 class Dataset:
-  def __init__(self, input_type, input_list, mc=False, mc_level = 7):
-    super().__init__()
-    self.data = []
-    if input_type == 'pc_normal':
-      for input_path in input_list:
-        # load npy
-        input_data = Image.open(input_path)
-        tdata = asarray(input_data)
-        cur_data = np.load(tdata)
-        # sample 4096
-        assert cur_data.shape[0] >= 8192, "input pc_normal should have at least 4096 points"
-        idx = np.random.choice(cur_data.shape[0], 8192, replace=False)
-        cur_data = cur_data[idx]
-        self.data.append({'pc_normal': cur_data, 'uid': input_path.split('/')[-1].split('.')[0]})
+    def __init__(self, input_type, input_list, mc=False, mc_level=7):
+        super().__init__()
+        self.data = []
+        if input_type == "pc_normal":
+            for input_path in input_list:
+                # load npy
+                # input_data = Image.open(input_path)
+                # tdata = asarray(input_data)
+                # cur_data = np.load(tdata)
+                cur_data = np.load(input_path)
+                # sample 4096
+                assert (
+                    cur_data.shape[0] >= 8192
+                ), "input pc_normal should have at least 4096 points"
+                idx = np.random.choice(cur_data.shape[0], 8192, replace=False)
+                cur_data = cur_data[idx]
+                self.data.append(
+                    {
+                        "pc_normal": cur_data,
+                        "uid": input_path.split("/")[-1].split(".")[0],
+                    }
+                )
 
-    elif input_type == 'mesh':
-      mesh_list = []
-      for input_path in input_list:
-        # load ply
-        cur_data = trimesh.load(input_path)
-        mesh_list.append(cur_data)
-      if mc:
-        print("First Marching Cubes and then sample point cloud, need several minutes...")
-      pc_list, _ = process_mesh_to_pc(mesh_list, marching_cubes=mc, mc_level=mc_level)
-      for input_path, cur_data in zip(input_list, pc_list):
-        self.data.append({'pc_normal': cur_data, 'uid': input_path.split('/')[-1].split('.')[0]})
-    print(f"dataset total data samples: {len(self.data)}")
+        elif input_type == "mesh":
+            mesh_list = []
+            for input_path in input_list:
+                # load ply
+                cur_data = trimesh.load(input_path)
+                mesh_list.append(cur_data)
+            if mc:
+                print(
+                    "First Marching Cubes and then sample point cloud, need several minutes..."
+                )
+            pc_list, _ = process_mesh_to_pc(
+                mesh_list, marching_cubes=mc, mc_level=mc_level
+            )
+            for input_path, cur_data in zip(input_list, pc_list):
+                self.data.append(
+                    {
+                        "pc_normal": cur_data,
+                        "uid": input_path.split("/")[-1].split(".")[0],
+                    }
+                )
+        print(f"dataset total data samples: {len(self.data)}")
