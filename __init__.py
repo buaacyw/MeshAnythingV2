d7c3390b0cfdf285d53fc5059f2260ac6e2111c0
SpiffGreen <spiffjekeygreen@gmail.com>
2024-09-04 16:08:26 +0100

    use importlib spec

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
+import importlib.util
 import os
 import sys
 import folder_paths
+import importlib
 
 ROOT_PATH = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui_meshanything_v2")
 MODULE_PATH = os.path.join(ROOT_PATH, "MeshAnything")
+
+spec = importlib.util.spec_from_loader("comfyui_meshanything_v2", ROOT_PATH)
+importlib.util.module_from_spec(spec)
+
 
 sys.path.append(ROOT_PATH)
 sys.path.append(MODULE_PATH)
