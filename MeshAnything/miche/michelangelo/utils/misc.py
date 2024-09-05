172f4a130d301ff7c0556be087377c17d5f80efc
SpiffGreen <spiffjekeygreen@gmail.com>
2024-09-05 04:37:33 +0100

    fix: add print for syspath

--- MeshAnything/miche/michelangelo/utils/misc.py
+++ MeshAnything/miche/michelangelo/utils/misc.py
@@ -18,25 +18,6 @@ def get_obj_from_str(string, reload=False):
     print("***********************************")
     print(module, cls)
     print("***********************************")
-    print(sys.path)
-
-    custom_module_path = '/home/qblocks/ComfyUI/custom_nodes/comfyui-meshanything-v2'
-
-    # List to hold custom modules
-    custom_modules = []
-
-    # Iterate through the sys.modules dictionary
-    for module_name, mod in sys.modules.items():
-        # Check if the module has a __file__ attribute and if it is in the custom path
-        if hasattr(mod, '__file__'):
-            module_file = os.path.abspath(mod.__file__)
-            if module_file.startswith(custom_module_path):
-                custom_modules.append(module_name)
-
-    # Print the list of custom modules
-    print("Custom Modules:")
-    for mod in sorted(custom_modules):
-        print(mod)
 
     ROOT_PATH = os.path.join(folder_paths.base_path, "custom_nodes", "comfyui_meshanything_v2", "MeshAnything")
 
