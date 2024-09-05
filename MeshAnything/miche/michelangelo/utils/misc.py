24ee44797083ace535b92e06f0aabc501aebc64e
SpiffGreen <spiffjekeygreen@gmail.com>
2024-09-05 04:42:34 +0100

    fix: add print for syspath

--- MeshAnything/miche/michelangelo/utils/misc.py
+++ MeshAnything/miche/michelangelo/utils/misc.py
@@ -19,7 +19,7 @@ def get_obj_from_str(string, reload=False):
     print(module, cls)
     print("***********************************")
 
-    ROOT_PATH = os.path.join(folder_paths.base_path, "custom_nodes", "comfyui_meshanything_v2", "MeshAnything", "__init__.py")
+    ROOT_PATH = os.path.join(folder_paths.base_path, "custom_nodes", "comfyui-meshanything-v2", "MeshAnything", "__init__.py")
 
     spec = importlib.util.spec_from_file_location("MeshAnything", ROOT_PATH)
     print("Spec", spec)
